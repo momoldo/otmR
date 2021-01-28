d95e0c7a901febb515c8a18193d147a847854881
momoldo <37210369+momoldo@users.noreply.github.com>
2021-01-28 17:21:27 +0900

    新規の関数

---
+++ R/otBasicStats.R
@@ -0,0 +1,28 @@
+# Calculate skewness for SPSS type
+#' @importFrom e1071 skewness
+Skewness <- function(x){
+  e1071::skewness(x, na.rm = TRUE, type = 2)
+}
+
+# Calculate kurtosis for SPSS type
+#' @importFrom e1071 kurtosis
+Kurtosis <- function(x){
+  e1071::kurtosis(x, na.rm = TRUE, type = 2)
+}
+
+#' Calculating Basic Statistics for ratio scale variables
+#'
+#' @param data target data.frame
+#'
+#' @importFrom magrittr %>%
+#' @importFrom e1071 skewness
+#' @importFrom e1071 kurtosis
+#' @importFrom modelsummary datasummary
+#' @importFrom kableExtra kable_classic
+#' @export
+#'
+otBasicStats <- function(data){
+  data %>% datasummary(All(.)~N+Min+Max+Mean+Median+SD+Skewness+Kurtosis+Histogram, data = .,
+                       output = "kableExtra", title = "Basic Statistics") %>%
+    kable_classic(full_width = FALSE)
+}
---
+++ R/ot基本統計量.R
@@ -0,0 +1,11 @@
+#' 基本的な統計量を計算する
+#'
+#' 量的変数の基本的な統計量を計算する．関数 otBasicStats の
+#' 日本語版．
+#'
+#' @param data 統計量を計算するdata.frame
+#' @export
+#'
+ot基本統計量 <- function(data){
+  otBasicStats(data)
+}
