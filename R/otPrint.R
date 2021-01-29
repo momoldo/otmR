f219b167796fc2dd47fbc325852c9e6b55f239ab
momoldo <37210369+momoldo@users.noreply.github.com>
2021-01-30 01:19:22 +0900

    変更
    相関係数に関する関数の出力関数を作成したことによる変更

--- R/otPrint.R
+++ R/otPrint.R
@@ -1,4 +1,4 @@
-# Print out function "otBasicStatis"
+#' Print out function "otBasicStatis"
 #'
 #' @importFrom kableExtra kbl kable_classic
 #' @param otm_obj any otmR object
@@ -11,6 +11,36 @@ ot_print_basic_stats <- function(otm_obj, ...){
   kbl(otm_obj, digits = dg, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
 }
 
+#' Function to ouput "otCorrelation" object
+#'
+#' @importFrom kableExtra kbl kable_classic cell_spec
+#' @param otm_obj any otmR object computed with "otCorrelation"
+#'
+ot_print_correlation <- function(otm_obj, ...){
+  dg <- ifelse(is.null(list(...)[["digits"]]),getOption("digits"),
+               as.integer(list(...)[["digits"]]))
+  tab_caption <- ifelse(is.null(list(...)[["caption"]]),"Correlation Matrix",
+                        list(...)[["caption"]])
+  is.colored <- ifelse(is.null(list(...)[["is.colored"]]), TRUE,
+                       as.logical(list(...)[["is.colored"]]))
+  if (is.colored){
+    S <- format(round(otm_obj, dg), nsmall = dg)
+    p.value <- attr(otm_obj, "otmR_P")
+    for ( i in 1:nrow(otm_obj)){
+      for ( j in 1:ncol(otm_obj)){
+        if (p.value[i,j]<0.05) {
+          S[i,j] <- cell_spec(S[i,j], background =  "lightgreen")
+        } else {
+          S[i,j] <- cell_spec(S[i,j], background = "white")
+        }
+      }
+    }
+    kbl(S, escape = F, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
+  } else {
+    kbl(otm_obj, digits = dg, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
+  }
+}
+
 #' Print out function for otmR
 #'
 #' @param otm_obj An object made by ot_functions
@@ -21,7 +51,8 @@ otPrint <- function(otm_obj, ...){
     func_name <- attr(otm_obj, "otmR_func")
     if (!is.null(func_name)){
       switch (func_name,
-              "BasicStats" = ot_print_basic_stats(otm_obj, ...),
+              "BasicStats"  = ot_print_basic_stats(otm_obj, ...),
+              "Correlation" = ot_print_correlation(otm_obj, ...),
               print(otm_obj)
       )
     }
