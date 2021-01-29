5d53462340d67176b03fb7d43bec78cf39df2b01
momoldo <37210369+momoldo@users.noreply.github.com>
2021-01-30 02:53:46 +0900

    追加
    関数 otCorrelation の出力を追加

--- R/otPrint.R
+++ R/otPrint.R
@@ -11,6 +11,34 @@ ot_print_basic_stats <- function(otm_obj, ...){
   kbl(otm_obj, digits = dg, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
 }
 
+# Print out function for "otCorrelation"
+#'
+#' @importFrom kableExtra kbl kable_classic cell_spec
+#' @param otm_obj any otmR object computed by "otCorrelation"
+#'
+ot_print_colleration <- function(otm_obj, ...){
+  dg <- ifelse(is.null(list(...)[["digits"]]),getOption("digits"),
+               as.integer(list(...)[["digits"]]))
+  tab_caption <- ifelse(is.null(list(...)[["caption"]]),"Correlation Matrix",
+                        list(...)[["caption"]])
+  is.colored <- ifelse(is.null(list(...)[["is.colored"]]), TRUE,
+                       as.logical(list(...)[["is.colored"]]))
+  if (is.colored){
+    S <- format(round(otm_obj, dg), nsmall = dg)
+    P <- attr(otm_obj, "otmR_P")
+    for (i in 1:nrow(S)){
+      for (j in 1:ncol(S)){
+        if (P[i,j]<0.05){
+          S[i,j] <- cell_spec(S[i,j], background = "lightgreen")
+        }
+      }
+    }
+    kbl(S, escape = FALSE, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
+  } else {
+    kbl(otm_obj, digits = dg, caption = tab_caption, align = "r") %>% kable_classic(full_width=FALSE)
+  }
+}
+
 #' Print out function for otmR
 #'
 #' @param otm_obj An object made by ot_functions
