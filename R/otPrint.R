682322e3fc83b408c0d61725b194b44b3870305b
momoldo <37210369+momoldo@users.noreply.github.com>
2021-02-01 22:37:24 +0900

    ロジスティック回帰の結果出力を追加

--- R/otPrint.R
+++ R/otPrint.R
@@ -50,7 +50,7 @@ ot_print_colleration <- function(otm_obj, ...){
 ot_print_glm <- function(otm_obj, ...){
   dg <- ifelse(is.null(list(...)[["digits"]]),getOption("digits"),
                as.integer(list(...)[["digits"]]))
-  tab_caption <- ifelse(is.null(list(...)[["caption"]]),"Correlation Matrix",
+  tab_caption <- ifelse(is.null(list(...)[["caption"]]),"Glm Result",
                         list(...)[["caption"]])
   fit <- attr(otm_obj, "otmR_fit")
 
@@ -61,6 +61,28 @@ ot_print_glm <- function(otm_obj, ...){
                               format(round(fit$F.value,3), nsmall = 3),
                               ", p=",format(round(pf(fit$F.value,fit$df_1,fit$df_2, lower.tail = FALSE),3), nsmall = 3)),
              general_title = "Note:")
+}
+
+#' Print "otLogisticRegression" results
+#'
+#' @importFrom kableExtra kbl kable_classic footnote
+#' @param otm_obj an object computed by "otLogisticRegression"
+#' @param ... further arguments passed to or from other methods.
+#'
+ot_print_logistic_regression <- function(otm_obj, ...){
+  dg <- ifelse(is.null(list(...)[["digits"]]),getOption("digits"),
+               as.integer(list(...)[["digits"]]))
+  tab_caption <- ifelse(is.null(list(...)[["caption"]]),"Logistic Regression Result",
+                        list(...)[["caption"]])
+  fit <- attr(otm_obj, "otmR_fit")
+
+  kbl(otm_obj, digits = dg, caption = tab_caption, align = "r") %>%
+    kable_classic(full_width=FALSE)
+#    footnote(general = paste0("R2=",format(round(fit$r.square,3), nsmall = 3),", F(",
+#                              fit$df_1,",",fit$df_2,")=",
+#                              format(round(fit$F.value,3), nsmall = 3),
+#                              ", p=",format(round(pf(fit$F.value,fit$df_1,fit$df_2, lower.tail = FALSE),3), nsmall = 3)),
+#             general_title = "Note:")
 }
 
 #' Print out function for otmR
@@ -77,6 +99,7 @@ otPrint <- function(otm_obj, ...){
               "BasicStats"  = ot_print_basic_stats(otm_obj, ...),
               "Correlation" = ot_print_colleration(otm_obj, ...),
               "Glm"         = ot_print_glm(otm_obj, ...),
+              "LogisticRegression" = ot_print_logistic_regression(otm_obj, ...),
               print(otm_obj)
       )
     }
