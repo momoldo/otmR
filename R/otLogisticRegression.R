f15434880a2416de590cc942c099a750f29e6e02
Akira Harada <37210369+momoldo@users.noreply.github.com>
2021-02-01 12:56:08 +0900

    新規作成

---
+++ R/otLogisticRegression.R
@@ -0,0 +1,29 @@
+#' Logistic Regression
+#'
+#' otLogisticRegression is used to fit logistic models,
+#' using glm functions.  An independent variable must have
+#' 0-1 value.
+#'
+#' @importFrom purrr modify
+#' @importFrom stats model.frame glm binomial
+#' @param data a data.frame object including both a dependent variables and independent
+#'   variables.
+#' @param model an object of class "formula": a symbolic description
+#'   of the model to be fitted.
+#' @param is.residual a logical value indicating whether return object
+#'   have residual information or not.
+#' @export
+#'
+otLogisticRegression <- function(data, model=NULL, is.residual=FALSE){
+  if (!is.null(model)){
+    d <- data.frame(model.frame(model, data = data))
+    res.fit <- glm(model, data = d, family = binomial(link = "logit"))
+    res.summary <- summary(res.fit)
+    res <- data.frame(beta = res.fit$coefficients,
+                      odds = exp(c(NA, res.fit$coefficients[2:length(model.glm$coefficients)])),
+                      std.err = res.summary$coefficients[,2],
+                      z.score = res.summary$coefficients[,3],
+                      p.value = res.summary$coefficients[,4])
+    attr(res, "otmR_func") <- "LogisticRegression"
+  }
+}
