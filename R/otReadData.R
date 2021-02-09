51e1a4552d47376a5def93505fa1c8dbd93a7823
momoldo <37210369+momoldo@users.noreply.github.com>
2021-02-09 16:35:47 +0900

    分散分析の実装に向けて因子型をどう扱うか

--- R/otReadData.R
+++ R/otReadData.R
@@ -6,10 +6,26 @@
 #'
 #' @importFrom readxl read_excel
 #' @param path path to the xls/xlsx file.
-#' @param sheet sheet to read.  Ether a string (the name of sheet),
+#' @param sheet sheet to read data.  Ether a string (the name of sheet),
 #'   or an integer (the position of the sheet).
+#' @param label_sheet sheet to read value label. Ether a string or an integer.
 #' @param ... further arguments passed to or from other methods.
 #' @export
-otReadData <- function(path, sheet = NULL, ...){ # not yet...
-  data <- read_excel(path, sheet, ...)
+otReadData <- function(path, sheet = NULL, label_sheet = "Label", ...){ # not yet...
+  if (file.exists(path)){
+    data   <- read_excel(path, sheet = sheet, ...)
+    sheets <- read_excel(path)
+    lab <- NULL
+
+    if (!is.na(match(label_sheet, sheets))){
+      lab  <- read_excel(path, sheet = label_sheet)
+    }
+
+    if (!is.null(lab)){
+      lab <- gsub("[[:blank:]]","",lab) # delete white spaces and tab
+    }
+    return(data)
+  } else {
+    return(NULL)
+  }
 }
