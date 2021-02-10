7d780b5174e630872d30e028cd1450adc2d809ab
momoldo <37210369+momoldo@users.noreply.github.com>
2021-02-10 21:46:03 +0900

    ラベル付き変数の読込に対応するための新規・追加

--- R/otReadData.R
+++ R/otReadData.R
@@ -4,25 +4,29 @@
 #' If a sheet2 on a xlsx book file includes descriptions of each variables,
 #' this function mutates a numeric vector to a factor one.
 #'
-#' @importFrom readxl read_excel
+#' @importFrom readxl read_excel excel_sheets
 #' @param path path to the xls/xlsx file.
 #' @param sheet sheet to read data.  Ether a string (the name of sheet),
 #'   or an integer (the position of the sheet).
-#' @param label_sheet sheet to read value label. Ether a string or an integer.
+#' @param label_file path to the xls/xlsx file to read value labels.
+#' @param label_sheet sheet to read value labels. Ether a string or an integer.
 #' @param ... further arguments passed to or from other methods.
 #' @export
-otReadData <- function(path, sheet = NULL, label_sheet = "Label", ...){ # not yet...
+#'
+otReadData <- function(path, sheet = NULL,
+                       label_file = path, label_sheet = "Label", ...){
   if (file.exists(path)){
     data   <- read_excel(path, sheet = sheet, ...)
-    sheets <- read_excel(path)
-    lab <- NULL
 
-    if (!is.na(match(label_sheet, sheets))){
-      lab  <- read_excel(path, sheet = label_sheet)
-    }
-
-    if (!is.null(lab)){
-      lab <- gsub("[[:blank:]]","",lab) # delete white spaces and tab
+    if(file.exists(label_file)){
+      sheets <- excel_sheets(path)
+      if (!is.na(match(label_sheet, sheets))){
+        lab  <- read_excel(label_file, sheet = label_sheet)
+        for (i in 1:nrow(lab)){
+          var_name <- as.character(lab[i,1])
+          data[,var_name] <- ot_set_variable(data[[var_name]], v_label = lab[i,-1])
+        }
+      }
     }
     return(data)
   } else {
--- R/ot_set_variable.R
+++ R/ot_set_variable.R
@@ -9,7 +9,8 @@
 #'
 ot_set_variable <- function(x, v_label=NULL){
   if (!is.null(v_label)){
-    df_label <- strsplit(gsub("[[:blank:]]","",v_label), split = "=") %>%
+    df_label <- v_label[!is.na(v_label)]
+    df_label <- strsplit(gsub("[[:blank:]]","",df_label), split = "=") %>%
       purrr::map_dfr(~data.frame(lv=.x[1],lb=.x[2]))
     res <- factor(x, levels = df_label[,1], labels = df_label[,2])
     return(res)
