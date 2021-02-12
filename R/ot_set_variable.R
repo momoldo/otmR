bb4913ff5b7c42f41306d1227fe5103407e0c8d5
momoldo <37210369+momoldo@users.noreply.github.com>
2021-02-12 19:14:14 +0900

    文字列ラベルへの対応

--- R/ot_set_variable.R
+++ R/ot_set_variable.R
@@ -9,10 +9,16 @@
 #'
 ot_set_variable <- function(x, v_label=NULL){
   if (!is.null(v_label)){
-    df_label <- v_label[!is.na(v_label)]
-    df_label <- strsplit(gsub("[[:blank:]]","",df_label), split = "=") %>%
+    df_label <- v_label[!is.na(v_label)] # remove NA
+    df_label <-
+      strsplit(gsub("[[:blank:]]","",df_label), split = "=") %>% # remove White Space
       purrr::map_dfr(~data.frame(lv=.x[1],lb=.x[2]))
-    res <- factor(x, levels = df_label[,1], labels = df_label[,2])
+
+    if (sum(is.na(df_label[,2]))>0){ # not "level=label" format
+      res <- factor(x, labels = df_label[,1])
+    } else { # "level=label" format
+      res <- factor(x, levels = df_label[,1], labels = df_label[,2])
+    }
     return(res)
   } else {
     return(x)
